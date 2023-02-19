91231e332a0627a645f366b91ad1e80131b4c5cd
Thorsten Schocke <tschocke@gmx.de>
2023-02-20 00:58:41 +0100

    formatting, some content

--- js/app.js
+++ js/app.js
@@ -25,28 +25,26 @@
 // NameSpace - glue all definitions to a long, unique object identifier
 // this is only viable as long as no this for an instance related behaviour is needed - ES6 style arrow functions
 // are not meant to be methods ( source MDN ) 
-var LandingPage_nameSpace = {};
+
 // Nobody wants to type that more than twice, shortcut: n
-let n = LandingPage_nameSpace;
-//n.activeElement = "dom object";
-n.liFragment = document.createDocumentFragment();
-// console.log(" Fragment :" + n.liFragment + "\n");
-n.currentActiveLink = "";
-n.prevActiveLink = "";
-n.scrollFlag = null;
+
+let liFragment = document.createDocumentFragment();
+
+let currentActiveLink = "";
+let prevActiveLink = "";
+let scrollFlag = null;
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
 
-n.getNavListParent = () => {
+const getNavListParent = () => {
   return document.getElementById("navbar__list");
 }
 
-//console.log(" element: " + n.getNavListParent() );
 
-n.logData = (data) => { console.log(" Out: " + data); }
+const logData = (data) => { console.log(" Out: " + data); }
 
 /**
  * End Helper Functions
@@ -55,7 +53,7 @@ n.logData = (data) => { console.log(" Out: " + data); }
 */
 // build the nav
 
-n.getSections = () => {
+const getSections = () => {
   let _sectionList = document.querySelectorAll("Section");
   let sectionElementList = [];
   for (let _liElement of _sectionList) {
@@ -64,21 +62,21 @@ n.getSections = () => {
   return sectionElementList;
 };
 
-n.removeActiveSectionClasses = () => {
+const removeActiveSectionClasses = () => {
   const _objectList = document.querySelectorAll("section");
   _objectList.forEach(function (element) { element.classList.remove("your-active-class"); })
 }
 
 // always add the homebutton first
-let _homeBtn = document.createElement("li");
-let _homeSymbol = document.createTextNode(  " Home " );
-_homeBtn.append(_homeSymbol);
-_homeBtn.id = "li__page_top";
-_homeBtn.classList.add("homeBtn", "unselectable");
-n.liFragment.appendChild(_homeBtn);
+// let _homeBtn = document.createElement("li");
+// let _homeSymbol = document.createTextNode(" Top ");
+// _homeBtn.append(_homeSymbol);
+// _homeBtn.id = "li__page_top";
+// _homeBtn.classList.add("homeBtn", "unselectable");
+// liFragment.appendChild(_homeBtn);
 
 // this method works by appending the created nav elements to namespace wide defined liFragment 
-n.mkNav = (sectionElement) => {
+const mkNav = (sectionElement) => {
   let _tempLi = document.createElement("li");
   let _tempName = document.createTextNode(" " + sectionElement.dataset.nav + " ");
   _tempLi.append(_tempName);
@@ -86,61 +84,64 @@ n.mkNav = (sectionElement) => {
   // let's store the reference to the originator for later use:
   _tempLi.dataset.ref = sectionElement.id;
   _tempLi.classList.add("menu__link", "unselectable");
-  n.liFragment.appendChild(_tempLi);
+  liFragment.appendChild(_tempLi);
 }
-// getSections returns an array of DOM Elements - mkNav is called on all of them to produce html nodes
-n.getSections().forEach(n.mkNav);
-// move the fragment into the DOM
-n.getNavListParent().appendChild(n.liFragment);
 
-// link to top of page this is not part of the section nav, rather decoration
-document.getElementById("li__page_top").addEventListener("click", (e) => { 
-  e.preventDefault();
-  window.scrollTo({top: 0, behavior: "smooth"});
- });
+// when most of the content we need is loaded start the menu build
+window.addEventListener('DOMContentLoaded', (e) => {
+  // getSections returns an array of DOM Elements - mkNav is called on all of them to produce html nodes
+  getSections().forEach(mkNav);
+  // move the fragment into the DOM
+  getNavListParent().appendChild(liFragment);
 
-// using one handle to scroll to element.id via dataset ref as parameter handshake
-// toggling state vars prevActiveLink and currentActiveLink 
+  // link to top of page this is not part of the section nav, rather decoration
+  document.getElementById("li__page_top").addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
 
-n.getNavListParent().addEventListener("click", (e) => {
-  e.preventDefault();
-  //console.log(" id : " + e.target.id);
-  if (e.target.dataset.ref != null) {
-    n.prevActiveLink = n.currentActiveLink;
-    n.currentActiveLink = e.target.id;
-    if (!e.target.classList.contains("nav__active__class")) {
-      e.target.classList.add("nav__active__class");
-    }
-    // prev + clicked id need to be different to not re-delete same active link clicked twice
-    if (n.prevActiveLink && (n.prevActiveLink != e.target.id)) {
-      document.getElementById(n.prevActiveLink).classList.remove("nav__active__class");
-    }
-    const originatorID = e.target.dataset.ref;
-    const origElement = document.getElementById(originatorID);
-    origElement.scrollIntoView({ behavior: "smooth", inline: "nearest" });
-  } else { return; }
-  //console.log('Target of event in navi : ' + e.target.dataset.ref);
+  // using one handle to scroll to element.id via dataset ref as parameter handshake
+  // toggling state vars prevActiveLink and currentActiveLink 
+
+  getNavListParent().addEventListener("click", (e) => {
+    e.preventDefault();
+    //console.log(" id : " + e.target.id);
+    if (e.target.dataset.ref != null) {
+      prevActiveLink = currentActiveLink;
+      currentActiveLink = e.target.id;
+      if (!e.target.classList.contains("nav__active__class")) {
+        e.target.classList.add("nav__active__class");
+      }
+      // prev + clicked id need to be different to not re-delete same active link clicked twice
+      if (prevActiveLink && (prevActiveLink != e.target.id)) {
+        document.getElementById(prevActiveLink).classList.remove("nav__active__class");
+      }
+      const originatorID = e.target.dataset.ref;
+      const origElement = document.getElementById(originatorID);
+      origElement.scrollIntoView({ behavior: "smooth", inline: "nearest" });
+    } else { return; }
+    //console.log('Target of event in navi : ' + e.target.dataset.ref);
+  });
 });
-
 // Add class 'active' to section when near top of viewport
 //  this eventListener needs to be aware scrolling / halting and interruptive scrolling
 // the setTimeout placeholder is undefined at loading time and "pushes" its timer forward while
 // the event keeps fireing
 
-
 window.addEventListener("scroll", function (e) {
-  if (n.scrollFlag !== null) {
-    clearTimeout(n.scrollFlag);
+  if (scrollFlag !== null) {
+    clearTimeout(scrollFlag);
   }
+
   //must be calculated ongoingly
-  n.viewportY = window.innerHeight;
-  //console.log("viewportY: " + n.viewportY );
-  n.scrollFlag = setTimeout(function () {
-    n.getSections().forEach(
+  let viewportY = window.innerHeight;
+  scrollFlag = setTimeout(function () {
+
+    getSections().forEach(
       function (sectionElement) {
         let yDistance = sectionElement.getBoundingClientRect().y;
         // Set sections as active in nav
-        if (yDistance > - 150 && yDistance < n.viewportY / 2) {
+        if (yDistance > - 150 && yDistance < viewportY / 2) {
           sectionElement.classList.add("active__section");
           document.getElementById("li__" + sectionElement.id).classList.add("nav__active__class");
         } else {
@@ -152,5 +153,3 @@ window.addEventListener("scroll", function (e) {
   }, 10);
 }, false);
 
-
-//todo add eventL. to document loaded   & document change for menu creation method
